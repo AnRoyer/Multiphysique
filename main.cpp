0f417beec5acfc7e768a5a61709855f587498907
Romain Boman <romain.boman@gmail.com>
2016-03-25 08:45:40 +0100

    ajout cmakelist en vue de l'utilisation de mumps

--- main.cpp
+++ main.cpp
@@ -106,9 +106,9 @@ int main(int argc, char **argv)
 
     FILE *fp = fopen("dataMatlabT.dat", "w");
 
-    std::map<Node*, std::vector<double> >::iterator itT = solutionT.begin();
+    std::map<Node*, std::vector<double> >::iterator itT = solutionTemperature.begin();
 
-    for(itT = solutionT.begin(); itT != solutionT.end(); itT++)
+    for(itT = solutionTemperature.begin(); itT != solutionTemperature.end(); itT++)
     {
         fprintf(fp, "%.15f \t %.15f \t %.15f \n", itT->first->x, itT->first->y, itT->second[0]);
     }
